5840127cb198873caabacebffc6b0f0219e271ab
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-11-23 16:55:26 +0000

    integrate default structure
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@1650 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/Results.class.php
+++ actions/Results.class.php
@@ -1,11 +1,235 @@
 <?php
+require_once('tao/actions/CommonModule.class.php');
+require_once('tao/actions/TaoModule.class.php');
 
-class Results extends Module {
+/**
+ * Results Controller provide actions performed from url resolution
+ * 
+ * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @package taoResults
+ * @subpackage actions
+ */
+class Results extends TaoModule {
 
-	public function index(){
-		$this->setData('testVar', "this is the ". get_class($this) ." module");
+	/**
+	 * constructor: initialize the service and the default data
+	 * @return Results
+	 */
+	public function __construct(){
 		
-		$this->setView('view.tpl');
+		parent::__construct();
+		
+		//the service is initialized by default
+		$this->service = tao_models_classes_ServiceFactory::get('Results');
+		$this->defaultData();
 	}
+	
+/*
+ * conveniance methods
+ */
+	
+	/**
+	 * get the instancee of the current subject regarding the 'uri' and 'classUri' request parameters
+	 * @return core_kernel_classes_Resource the subject instance
+	 */
+	private function getCurrentResult(){
+		
+		$uri = tao_helpers_Uri::decode($this->getRequestParameter('uri'));
+		if(is_null($uri) || empty($uri)){
+			throw new Exception("No valid uri found");
+		}
+		
+		$clazz = $this->getCurrentClass();
+		
+		$result = $this->service->getResult($uri, 'uri', $clazz);
+		if(is_null($result)){
+			throw new Exception("No subject found for the uri {$uri}");
+		}
+		
+		return $result;
+	}
+	
+/*
+ * controller actions
+ */
+
+	/**
+	 * main action
+	 * @return void
+	 */
+	public function index(){
+		
+		if($this->getData('reload') == true){
+			unset($_SESSION[SESSION_NAMESPACE]['uri']);
+			unset($_SESSION[SESSION_NAMESPACE]['classUri']);
+		}
+		
+		$context = Context::getInstance();
+		$this->setData('content', "this is the ". get_class($this) ." module, " . $context->getActionName());
+		$this->setView('index.tpl');
+	}
+	
+	/**
+	 * Render json data to populate the subject tree 
+	 * 'modelType' must be in request parameter
+	 * @return void
+	 */
+	public function getResults(){
+		
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		$highlightUri = '';
+		if($this->hasSessionAttribute("showNodeUri")){
+			$highlightUri = $this->getSessionAttribute("showNodeUri");
+			unset($_SESSION[SESSION_NAMESPACE]["showNodeUri"]);
+		} 
+		echo json_encode($this->service->toTree( $this->service->getResultClass(), true, true, $highlightUri));
+	}
+	
+	/**
+	 * Add an subject instance
+	 */
+	public function addResult(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		$clazz = $this->getCurrentClass();
+		$result = $this->service->createInstance($clazz);
+		if(!is_null($result) && $result instanceof core_kernel_classes_Resource){
+			echo json_encode(array(
+				'label'	=> $result->getLabel(),
+				'uri' 	=> tao_helpers_Uri::encode($result->uriResource)
+			));
+		}
+	}
+	
+	/**
+	 * edit an subject instance
+	 */
+	public function editResult(){
+		$clazz = $this->getCurrentClass();
+		$result = $this->getCurrentResult();
+		$myForm = tao_helpers_form_GenerisFormFactory::instanceEditor($clazz, $result);
+		if($myForm->isSubmited()){
+			if($myForm->isValid()){
+				
+				$result = $this->service->bindProperties($result, $myForm->getValues());
+				
+				$this->setSessionAttribute("showNodeUri", tao_helpers_Uri::encode($result->uriResource));
+				$this->setData('message', 'Result saved');
+				$this->setData('reload', true);
+				$this->forward('Results', 'index');
+			}
+		}
+		
+		$this->setData('formTitle', 'Edit subject');
+		$this->setData('myForm', $myForm->render());
+		$this->setView('form.tpl');
+	}
+	
+	/**
+	 * add a subject model (subclass Result)
+	 */
+	public function addResultClass(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		$clazz = $this->service->createResultClass($this->getCurrentClass());
+		if(!is_null($clazz) && $clazz instanceof core_kernel_classes_Class){
+			echo json_encode(array(
+				'label'	=> $clazz->getLabel(),
+				'uri' 	=> tao_helpers_Uri::encode($clazz->uriResource)
+			));
+		}
+	}
+	
+	/**
+	 * Edit a subject model (edit a class)
+	 */
+	public function editResultClass(){
+		$myForm = $this->editClass($this->getCurrentClass(), $this->service->getResultClass());
+		if($myForm->isSubmited()){
+			if($myForm->isValid()){
+				if($clazz instanceof core_kernel_classes_Resource){
+					$this->setSessionAttribute("showNodeUri", tao_helpers_Uri::encode($clazz->uriResource));
+				}
+				$this->setData('message', 'class saved');
+				$this->setData('reload', true);
+				$this->forward('Results', 'index');
+			}
+		}
+		$this->setData('formTitle', 'Edit subject class');
+		$this->setData('myForm', $myForm->render());
+		$this->setView('form.tpl');
+	}
+	
+	/**
+	 * delete a subject or a subject model
+	 * called via ajax
+	 */
+	public function delete(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		
+		$deleted = false;
+		if($this->getRequestParameter('uri')){
+			$deleted = $this->service->deleteResult($this->getCurrentResult());
+		}
+		else{
+			$deleted = $this->service->deleteResultClass($this->getCurrentClass());
+		}
+		
+		echo json_encode(array('deleted'	=> $deleted));
+	}
+	
+	/**
+	 * duplicate a subject instance by property copy
+	 */
+	public function cloneResult(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		
+		$result = $this->getCurrentResult();
+		$clazz = $this->getCurrentClass();
+		
+		$clone = $this->service->createInstance($clazz);
+		if(!is_null($clone)){
+			
+			foreach($clazz->getProperties() as $property){
+				foreach($result->getPropertyValues($property) as $propertyValue){
+					$clone->setPropertyValue($property, $propertyValue);
+				}
+			}
+			$clone->setLabel($result->getLabel()."'");
+			echo json_encode(array(
+				'label'	=> $clone->getLabel(),
+				'uri' 	=> tao_helpers_Uri::encode($clone->uriResource)
+			));
+		}
+	}
+	
+	/*
+	 * @TODO implement the following actions
+	 */
+	
+	public function getMetaData(){
+		throw new Exception("Not yet implemented");
+	}
+	
+	public function saveComment(){
+		throw new Exception("Not yet implemented");
+	}
+	
+	public function import(){
+		throw new Exception("Not yet implemented");
+	}
+	
+	public function export(){
+		throw new Exception("Not yet implemented");
+	}
+
 }
 ?>
--- models/classes/class.ResultsService.php
+++ models/classes/class.ResultsService.php
@@ -9,10 +9,10 @@ error_reporting(E_ALL);
  *
  * This file is part of Generis Object Oriented API.
  *
- * Automatically generated on 14.09.2009, 15:14:24 with ArgoUML PHP module 
- * (last revised $Date: 2008-04-19 08:22:08 +0200 (Sat, 19 Apr 2008) $)
+ * Automatically generated on 23.11.2009, 14:25:39 with ArgoUML PHP module 
+ * (last revised $Date: 2009-04-11 21:57:46 +0200 (Sat, 11 Apr 2009) $)
  *
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  * @package taoResults
  * @subpackage models_classes
  */
@@ -25,7 +25,7 @@ if (0 > version_compare(PHP_VERSION, '5')) {
  * The Service class is an abstraction of each service instance. 
  * Used to centralize the behavior related to every servcie instances.
  *
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  */
 require_once('tao/models/classes/class.Service.php');
 
@@ -41,7 +41,7 @@ require_once('tao/models/classes/class.Service.php');
  * Short description of class taoResults_models_classes_ResultsService
  *
  * @access public
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  * @package taoResults
  * @subpackage models_classes
  */
@@ -53,22 +53,66 @@ class taoResults_models_classes_ResultsService
 
     // --- ATTRIBUTES ---
 
+    /**
+     * Short description of attribute resultClass
+     *
+     * @access protected
+     * @var Class
+     */
+    protected $resultClass = null;
+
+    /**
+     * Short description of attribute resultsOntologies
+     *
+     * @access protected
+     * @var array
+     */
+    protected $resultsOntologies = array('http://www.tao.lu/Ontologies/TAOResult.rdf');
+
     // --- OPERATIONS ---
 
     /**
-     * Short description of method getResultsByGroup
+     * Short description of method __construct
      *
-     * @access protected
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
-     * @param  Resource group
-     * @return core_kernel_classes_ContainerCollection
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @return mixed
      */
-    protected function getResultsByGroup( core_kernel_classes_Resource $group)
+    public function __construct()
+    {
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C75 begin
+		
+		parent::__construct();
+		$this->resultClass 	= new core_kernel_classes_Class( TAO_RESULT_CLASS );
+		$this->loadOntologies($this->resultsOntologies);
+		
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C75 end
+    }
+
+    /**
+     * Short description of method getResult
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  string identifier
+     * @param  string mode
+     * @param  Class clazz
+     * @return core_kernel_classes_Resource
+     */
+    public function getResult($identifier, $mode = 'uri',  core_kernel_classes_Class $clazz = null)
     {
         $returnValue = null;
 
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F3 begin
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F3 end
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C77 begin
+		
+		if(is_null($clazz)){
+			$clazz = $this->resultClass;
+		}
+		if($this->isResultClass($clazz)){
+			$returnValue = $this->getOneInstanceBy( $clazz, $identifier, $mode);
+		}
+		
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C77 end
 
         return $returnValue;
     }
@@ -77,7 +121,7 @@ class taoResults_models_classes_ResultsService
      * Short description of method getResults
      *
      * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
      * @param  array options
      * @return core_kernel_classes_ContainerCollection
      */
@@ -86,16 +130,203 @@ class taoResults_models_classes_ResultsService
         $returnValue = null;
 
         // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017CB begin
+		
+		if(is_null($clazz)){
+			$clazz = $this->resultClass;
+		}
+		
+		//verify the class type
+		if( $clazz->uriResource != $this->resultClass->uriResource ){
+			if( ! $clazz->isSubClassOf($this->resultClass) ){
+				throw new Exception("your clazz argument must referr to a Result or Result's subclass in your ontology ");
+			}
+		}
+		
+		$instances = $clazz->getInstances();
+		if($instances->count() > 0){
+			
+			//paginate options
+			//@todo implements
+			if(count($options) > 0){
+			
+				$sequence = $instances->sequence;
+				
+				if(isset($options['order'])){
+					//order sequence by $options['order']
+				}
+				if(isset($options['start'])){
+					//return sequence from $options['start'] index
+				}
+				if(isset($options['offset'])){
+					//return  $options['offset'] elements of the sequence
+				}
+			
+				$returnValue = new core_kernel_classes_ContainerCollection(new core_kernel_classes_Container(__METHOD__),__METHOD__);
+				$returnValue->sequence = $sequence;
+			}
+			else{
+				$returnValue = $instances;
+			}
+		}
+		
         // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017CB end
 
         return $returnValue;
     }
 
     /**
+     * Short description of method getResultClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  string uri
+     * @return core_kernel_classes_Class
+     */
+    public function getResultClass($uri = '')
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C86 begin
+		
+		if(empty($uri) && !is_null($this->resultClass)){
+			$returnValue = $this->resultClass;
+		}
+		else{
+			$clazz = new core_kernel_classes_Class($uri);
+			if($this->isResultClass($clazz)){
+				$returnValue = $clazz;
+			}
+		}
+		
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C86 end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method createResultClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @param  string label
+     * @param  array properties
+     * @return core_kernel_classes_Class
+     */
+    public function createResultClass( core_kernel_classes_Class $clazz = null, $label = '', $properties = array())
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C8C begin
+		
+		if(is_null($clazz)){
+			$clazz = $this->resultClass;
+		}
+		
+		if($this->isResultClass($clazz)){
+		
+			$resultClass = $this->createSubClass($clazz, $label);
+			
+			foreach($properties as $propertyName => $propertyValue){
+				$myProperty = $resultClass->createProperty(
+					$propertyName,
+					$propertyName . ' ' . $label .' result property created from ' . get_class($this) . ' the '. date('Y-m-d h:i:s') 
+				);
+				
+				//@todo implement check if there is a widget key and/or a range key
+			}
+			$returnValue = $resultClass;
+		}
+		
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C8C end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method deleteResult
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Resource result
+     * @return boolean
+     */
+    public function deleteResult( core_kernel_classes_Resource $result)
+    {
+        $returnValue = (bool) false;
+
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F6 begin
+		
+		if(!is_null($result)){
+			$returnValue = $result->delete();
+		}
+		
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F6 end
+
+        return (bool) $returnValue;
+    }
+
+    /**
+     * Short description of method deleteResultClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @return boolean
+     */
+    public function deleteResultClass( core_kernel_classes_Class $clazz)
+    {
+        $returnValue = (bool) false;
+
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C9E begin
+		
+		if(!is_null($clazz)){
+			if($this->isResultClass($clazz) && $clazz->uriResource != $this->resultClass->uriResource){
+				$returnValue = $clazz->delete();
+			}
+		}
+		
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001C9E end
+
+        return (bool) $returnValue;
+    }
+
+    /**
+     * Short description of method isResultClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @return boolean
+     */
+    public function isResultClass( core_kernel_classes_Class $clazz)
+    {
+        $returnValue = (bool) false;
+
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001CA2 begin
+		
+		if($clazz->uriResource == $this->resultClass->uriResource){
+			$returnValue = true;	
+		}
+		else{
+			foreach($this->getSubjectClasses() as $subclass){
+				if($clazz->uriResource == $subclass->uriResource){
+					$returnValue = true;
+					break;	
+				}
+			}
+		}
+		
+        // section 127-0-1-1--233123b3:125208ce1cc:-8000:0000000000001CA2 end
+
+        return (bool) $returnValue;
+    }
+
+    /**
      * Short description of method analyseResult
      *
      * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
      * @param  Resource result
      * @param  Resource analyseFilter
      * @return core_kernel_classes_Resource
@@ -111,21 +342,21 @@ class taoResults_models_classes_ResultsService
     }
 
     /**
-     * Short description of method deleteResult
+     * Short description of method getResultsByGroup
      *
-     * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
-     * @param  Resource result
-     * @return boolean
+     * @access protected
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Resource group
+     * @return core_kernel_classes_ContainerCollection
      */
-    public function deleteResult( core_kernel_classes_Resource $result)
+    protected function getResultsByGroup( core_kernel_classes_Resource $group)
     {
-        $returnValue = (bool) false;
+        $returnValue = null;
 
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F6 begin
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F6 end
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F3 begin
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017F3 end
 
-        return (bool) $returnValue;
+        return $returnValue;
     }
 
 } /* end of class taoResults_models_classes_ResultsService */
