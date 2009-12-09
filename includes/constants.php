370af7f7d167566df882fb1b30057e9b44116345
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-09 16:11:19 +0000

    update constants
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@1763 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,7 @@
 <?php
 $todefine = array(
-	'TAO_RESULT_CLASS' => 'http://www.tao.lu/Ontologies/TAOResult.rdf#Result'
+	'TAO_RESULT_CLASS' => 'http://www.tao.lu/Ontologies/TAOResult.rdf#Result',
+	'GENERIS_BOOLEAN'		=> 'http://www.tao.lu/Ontologies/generis.rdf#Boolean'
 );
 foreach($todefine as $constName => $constValue){
 	if(!defined($constName)){
