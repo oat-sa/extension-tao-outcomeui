e888c5b05599ad9e2095d3d6b587ed8e3e51bd4b
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-10 17:38:14 +0000

    clean config.php, most of them are now empty
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@1776 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -1,54 +1,15 @@
 <?php
 
-session_start();
 
 
-/**
- * this code is executed before all other script
- */
 
-# constants definition
-define('HTTP_GET', 		'GET');
-define('HTTP_POST', 	'POST');
-define('HTTP_PUT', 		'PUT');
-define('HTTP_DELETE', 	'DELETE');
-define('HTTP_HEAD', 	'HEAD');
-
-# all error
-error_reporting(E_ALL);
-
-# xdebug custom error reporting
-if (function_exists("xdebug_enable"))  {
-	xdebug_enable();
-}
 
 require_once 	dirname(__FILE__). "/config.php";
 require_once 	dirname(__FILE__). "/constants.php";
 
 
 
-/**
- * @function tao_autoload
- * permits to include classes automatically using the ARGOUML class naming convention
- * @param 	string		$pClassName		Name of the class
- */
-function tao_autoload($pClassName) {
 
-	$split = split("_",$pClassName);
-	$path = GENERIS_BASE_PATH.'/';
-	for ( $i = 0 ; $i<sizeof($split)-1 ; $i++){
-		$path .= $split[$i].'/';
-	}
-
-	$filePath = $path . 'class.'.$split[sizeof($split)-1] . '.php';
-	if (file_exists($filePath)){
-		require_once $filePath;
-	}
-
-}
-
-
-spl_autoload_register("tao_autoload");
 
 
 set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH);
