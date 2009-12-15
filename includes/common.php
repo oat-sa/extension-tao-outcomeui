11b16ff4a4b162c90eb55071ed6151d3179e2a3f
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-15 15:04:34 +0000

    integrate user auth
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@1804 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -1,22 +1,10 @@
 <?php
+session_start();
 
-
-
-
-
-require_once 	dirname(__FILE__). "/config.php";
-require_once 	dirname(__FILE__). "/constants.php";
-
-
-//$__classLoader->addPackage(DIR_ACTIONS);
-//$__classLoader->addPackage(DIR_HELPERS);
-//spl_autoload_register('common_Utils::registerAutoload');
-
+require_once dirname(__FILE__). '/config.php';
+require_once dirname(__FILE__). '/constants.php';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH.'/..');
 
-core_control_FrontController::connect(API_LOGIN, API_PASS, API_MODULE);
-core_kernel_classes_Session::singleton()->setLg($GLOBALS['lang']);
-core_kernel_classes_Session::singleton()->defaultLg = 'en';
-
+include_once 'tao/includes/prepend.php';
 ?>
