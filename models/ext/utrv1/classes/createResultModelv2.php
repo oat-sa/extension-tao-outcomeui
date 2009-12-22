8f52a7d6647cf586794cc1b9dd19f21490d08ab7
ydjaghloul <ydjaghloul@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-22 16:15:54 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@1910 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/ext/utrv1/classes/createResultModelv2.php
+++ models/ext/utrv1/classes/createResultModelv2.php
@@ -19,203 +19,7 @@ define ("ID_TEST",'ID_TEST');
 define ("RESULT_NS",'http://127.0.0.1/middleware/demo.rdf#');
 define ('RESULT_MODEL',$_SERVER['DOCUMENT_ROOT'].'/taoResults/models/ext/utrv1/classes/model.xml');
 
-
 //
-class importLog {
-    public $domRusult;
-    public $resultDom;
-
-    public function __construct($domSom) {//our dev teame Som
-        $logDom = new DOMDocument();
-        //$logDom->load(inputFile);
-        $logDom = $domSom;
-        $this->resultDom = $logDom;
-        createTestInstances();
-
-        return 'OK';
-
-    }
-
-    //    public function createDOM() {
-    //    //declarer le dom
-    //
-    //        $logDom = new DOMDocument();
-    //        $logDom->load(inputFile);
-    //        return $logDom;
-    //    }
-    //
-    //    public function getAttValueByXpath($nodePath, $att) {
-    //        $dom = new DOMDocument();
-    //        //get the actual dom
-    //        $dom = $this->domRusult;//$this->createDOM(inputFile);
-    //        //intialize th xpath
-    //        $xp = new DOMXPath($dom);
-    //        $node=$xp->query($nodePath);
-    //        $val = $node->item(0)->getAttribute($att);
-    //        return $val;
-    //
-    //    }
-    //
-    //    //get attribute value by tag name
-    //
-    //    public function getValueByNodeName() {
-    //
-    //
-    //    }
-    //
-    //    public function getAttValueDom($nodeName,$att) {
-    //        $dom = new DOMDocument();
-    //        $dom = $this->createDOM(inputFile);
-    //        $node = $dom->getElementsByTagName($nodeName);
-    //        $val = $node->item(0)->getAttribute($att);
-    //
-    //
-    //        return $val;
-    //
-    //    }
-    //    //Create an instance for the test class
-
-    public function createTestInstances() {
-
-
-    }
-
-    //The bigest method, to create the whole instances of all classes in the result model
-    public function createResultInstance() {
-        $dom = $this->resultDom;
-        //connect to generis
-//        define('API_LOGIN','generis');
-//        define('API_PASSWORD',md5('g3n3r1s'));
-
-        define('API_LOGIN','demo');
-        define('API_PASSWORD',md5('demo'));
-        core_control_FrontController::connect(API_LOGIN, API_PASSWORD, DATABASE_NAME);
-        //
-        //Get test attribute value
-        $testDom= $dom->getElementsByTagName("TEST")->item(0);
-
-        $ID_TEST =          $testDom->getAttribute("rdfid");
-
-        $LABEL_TEST=        $testDom->getAttribute("rdfs:Label");
-        $COMMENT_TEST=      $testDom->getAttribute("rdfs:Comment");
-        //*******************
-        $testDom= $dom->getElementsByTagName("HASSCORINGMETHOD")->item(0);
-        $HASSCORINGMETHOD_NAME=$testDom->getAttribute("tao:NAME");
-        //**********************
-        $testDom= $dom->getElementsByTagName("SCORE")->item(0);
-        $SCORE_VALUE=       $testDom->getAttribute("tao:VALUE");
-        //*******************
-        $testDom= $dom->getElementsByTagName("CUMULMODEL")->item(0);
-        $CUMULMODEL_NAME=   $testDom->getAttribute("tao:NAME");
-        //**********************
-        $testDom= $dom->getElementsByTagName("SUBJECT")->item(0);
-        $SUBJECT_ID =       $testDom->getAttribute("rdfid");
-        $SUBJECT_LABEL =    $testDom->getAttribute("rdfs:Label");
-        $TESTBEHAVIOR= '';     // puted after adding the instance in testbehavir class
-
-        //Create instance and property values
-
-        $class = new core_kernel_classes_Class(RESULT_NS."TEST_CLASS");
-        $instanceTest = $class->createInstance("test of student");
-        //put property values /**** test with 3 properties only
-
-        $propTest = new core_kernel_classes_Property(RESULT_NS."ID_TEST");
-        $instanceTest->setPropertyValue($propTest,$ID_TEST );
-
-        $propTest = new core_kernel_classes_Property(RESULT_NS."HASSCORINGMETHOD_NAME");
-        $instanceTest->setPropertyValue($propTest,$HASSCORINGMETHOD_NAME );
-
-        $propTest = new core_kernel_classes_Property(RESULT_NS."SUBJECT_ID");
-        $instanceTest->setPropertyValue($propTest,$SUBJECT_ID );
-
-
-
-        // echo $ID_TEST. " -- ". $CUMULMODEL_NAME. '--- '.$SUBJECT_LABEL;
-
-
-        $CITEM= '';            // puted after adding the instance to citem class
-
-
-        //Get Citem attribute
-        $citemDomList = $dom->getElementsByTagName("CITEM");//
-
-        foreach($citemDomList as $citemDom) {
-        //get attributes values
-            $WEIGHT = $citemDom->getAttribute("tao:WEIGHT");
-
-            $MODEL=$citemDom->getAttribute("tao:MODEL");
-
-            $DEFINITIONFILE=$citemDom->getAttribute("tao:DEFINITIONFILE");
-
-            $SEQUENCE=$citemDom->getAttribute("tao:SEQUENCE");
-
-            $ENDORSMENT=$citemDom->getAttribute("tao:ENDORSMENT");
-
-            $ITEMUSAGE=$citemDom->getAttribute("tao:ITEMUSAGE");
-
-            $ITEMBEHAVIOR='';//puted after adding the itemBehavior class
-            //*****************************************************
-
-            //Create instance of citem
-            $class = new core_kernel_classes_Class(RESULT_NS."CITEM_CLASS");
-            $instanceCitem = $class->createInstance("test of CITEM");
-            //put property values /**** test with 3 properties only
-
-            $propCitem = new core_kernel_classes_Property(RESULT_NS."WEIGHT");
-            $instanceCitem->setPropertyValue($propCitem,$WEIGHT );
-
-            $propCitem = new core_kernel_classes_Property(RESULT_NS."SEQUENCE");
-            $instanceCitem->setPropertyValue($propCitem,$SEQUENCE );
-
-            //*** The rage value of Test Instance/ THE Citem property
-            $CITEM = $instanceCitem->uriResource;
-            $propTest = new core_kernel_classes_Property(RESULT_NS."CITEM");
-            $instanceTest->setPropertyValue($propTest,$CITEM );
-
-
-            //echo "<br>".$WEIGHT."- ".$DEFINITIONFILE. " - ".$SEQUENCE;
-            //*******************************************************************************
-            //Get itemBehavior attribut values of the actual Citem
-
-            $itemBehaviorDomList = $citemDom->getElementsByTagName("ITEMBEHAVIOR");
-            foreach($itemBehaviorDomList as $itemBehaviorDom) {
-
-                $LISTENERNAME=$itemBehaviorDom->getAttribute("tao:LISTENERNAME");
-                $LISTENERVALUE=$itemBehaviorDom->getAttribute("tao:LISTENERNAME");
-
-                //Create Instances of itemBehavior
-                //Create instance of citem
-                $class = new core_kernel_classes_Class(RESULT_NS."ITEMBEHAVIOR_CLASS");
-                $instanceIB = $class->createInstance("test of ItemBehavior");
-
-                $propIB = new core_kernel_classes_Property(RESULT_NS."LISTENERNAME");
-                $instanceIB->setPropertyValue($propIB,$LISTENERNAME );
-
-                $propIB = new core_kernel_classes_Property(RESULT_NS."LISTENERVALUE");
-                $instanceIB->setPropertyValue($propIB,$LISTENERVALUE );
-                //The range of Citem instance / The ITEMBEHAVIOR
-                $ITEMBEHAVIOR = $instanceIB->uriResource;
-                $propCitem = new core_kernel_classes_Property(RESULT_NS."ITEMBEHAVIOR");
-                $instanceCitem->setPropertyValue($propCitem,$ITEMBEHAVIOR );
-
-
-                echo "<br>***** ". $LISTENERNAME;
-
-            }
-
-
-
-        }
-
-    //Get itemBehavior attribut value
-    //        $LISTENERNAME=$itemBehaviorDom->getAttribute("tao:LISTENERNAME");
-    //
-    //        $LISTENERVALUE=$itemBehaviorDom->getAttribute("tao:LISTENERNAME");
-
-    }
-
-
-}
 //This class build the result model, by creatin the differents classes abd properties,
 //to have the name space of the uri, we can cretae a FASTOCHE resource, get its uri and get also, the name space delimited by #
 //name space#uriuri
@@ -229,8 +33,11 @@ class ResultModelBuilder {
         $domResult->load(RESULT_MODEL);
         //get class info
 
-        $listClasses = $domResult->getElementsByTagName("class");
+        define('API_LOGIN','generis');
+        define('API_PASSWORD',md5('g3n3r1s'));
+        core_control_FrontController::connect(API_LOGIN, API_PASSWORD, DATABASE_NAME);
 
+        $listClasses = $domResult->getElementsByTagName("class");
 
         foreach($listClasses as $class) {
         //labelClass = $class->getAttribute("label");
@@ -264,6 +71,28 @@ class ResultModelBuilder {
             $this->createClassWithProperties($this->uriRootClassOfResult, '#'.$uriClass, $labelClass, '$comment', $t);
 
         }
+        //add range properties
+
+        $prop = new core_kernel_classes_Property(RESULT_NS."CITEM");
+        $class = new core_kernel_classes_Class(RESULT_NS."CITEM_CLASS");
+        $prop->setRange($class);
+
+        $prop = new core_kernel_classes_Property(RESULT_NS."ITEMBEHAVIOR");
+        $class = new core_kernel_classes_Class(RESULT_NS."ITEMBEHAVIOR_CLASS");
+        $prop->setRange($class);
+
+        //for subject antd other classes
+        $prop = new core_kernel_classes_Property(RESULT_NS."SUBJECT_ID");
+        $class = new core_kernel_classes_Class(TAO_SUBJECT_CLASS);
+
+        $prop->setRange($class);
+
+
+        $prop = new core_kernel_classes_Property(RESULT_NS."ID_TEST");
+        $class = new core_kernel_classes_Class(TAO_TEST_CLASS);
+        $prop->setRange($class);
+
+
 
     }
 
@@ -298,19 +127,10 @@ class ResultModelBuilder {
 
 
         }//end for each add properties
-
-        ///add range properties
-        $prop = new core_kernel_classes_Property(RESULT_NS."CITEM");
-        $class = new core_kernel_classes_Class(RESULT_NS."CITEM_CLASS");
-        $prop->setRange($class);
-
-        $prop = new core_kernel_classes_Property(RESULT_NS."ITEMBEHAVIOR");
-        $class = new core_kernel_classes_Class(RESULT_NS."ITEMBEHAVIOR_CLASS");
-        $prop->setRange($class);
-
         //Put the class as subclass of root
         $rootResultClass = new core_kernel_classes_Class($uriRootClass);
         $trClass->setSubClassOf($rootResultClass);
+
     //end
 
 
@@ -331,7 +151,7 @@ class ResultModelBuilder {
 //$t[]=$propDescription;
 
 $p= new ResultModelBuilder();
-echo $_SERVER['DOCUMENT_ROOT']."generis/common/inc.extension.php";
+//echo $_SERVER['DOCUMENT_ROOT']."generis/common/inc.extension.php";
 $p->createModel();
 
 
