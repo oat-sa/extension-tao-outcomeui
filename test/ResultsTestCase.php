e65b03eacda8893e5021e246d2f8db9f9e5dbc1a
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-16 13:29:42 +0000

    * Create unit test for extensions
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@2449 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ test/ResultsTestCase.php
@@ -0,0 +1,41 @@
+<?php
+require_once dirname(__FILE__) . '/../../tao/test/TestRunner.php';
+require_once dirname(__FILE__) . '/../includes/common.php';
+
+/**
+ *
+ * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @package taoResults
+ * @subpackage test
+ */
+class ResultsTestCase extends UnitTestCase {
+	
+	/**
+	 * 
+	 * @var taoResults_models_classes_ResultsService
+	 */
+	protected $resultsService = null;
+	
+	/**
+	 * tests initialization
+	 */
+	public function setUp(){		
+		TestRunner::initTest();
+	}
+	
+	/**
+	 * Test the user service implementation
+	 * @see tao_models_classes_ServiceFactory::get
+	 * @see taoResults_models_classes_ResultsService::__construct
+	 */
+	public function testService(){
+		
+		$resultsService = tao_models_classes_ServiceFactory::get('Results');
+		$this->assertIsA($resultsService, 'tao_models_classes_Service');
+		$this->assertIsA($resultsService, 'taoResults_models_classes_ResultsService');
+		
+		$this->resultsService = $resultsService;
+	}
+	
+}
+?>
