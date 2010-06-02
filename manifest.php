01b91be4c467829ecdda595894f36f48e897ffff
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-06-02 09:33:45 +0000

    change extension version in all manifest and in database
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@2731 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -9,7 +9,7 @@
 		'name' => 'TAO Results',
 		'description' => 'TAO Results extensions http://www.tao.lu',
 		'additional' => array(
-			'version' => '1.0',
+			'version' => '1.2',
 			'author' => 'CRP Henri Tudor',
 			'dependances' => array(),
 			'install' => array( 
