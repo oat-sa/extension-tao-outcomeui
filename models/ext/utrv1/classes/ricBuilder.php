82caffac2bea54fc38bce4b9c56faa59ac6f049b
ydjaghloul <ydjaghloul@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-08-16 08:28:20 +0000

    script to build the class of rev item test  problem
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@3084 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ models/ext/utrv1/classes/ricBuilder.php
@@ -0,0 +1,21 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author djaghloul
+ */
+require_once 'class.revItemCapacity.php';
+
+    //put your code here
+    $p = new classrevItemCapacity();
+    $p->buildModel();
+    
+
+
+
+?>
