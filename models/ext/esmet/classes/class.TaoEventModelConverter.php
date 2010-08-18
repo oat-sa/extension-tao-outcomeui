2bb75769cec40057434fb331cd2c664746773cbe
ydjaghloul <ydjaghloul@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-08-18 13:54:20 +0000

    Younes Djaghloul -Add new class class.TaoEventModelConverter.php to import traces from other system -Add support for HAWAI import,  the result of the conversion is a trace according Tao Event Model
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@3112 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ models/ext/esmet/classes/class.TaoEventModelConverter.php
@@ -0,0 +1,111 @@
+<?php
+
+require_once 'esmet_config.php';
+/*
+ * converts an iputed trace to event TAO Event Model
+ *
+ */
+
+/**
+ * Description of class
+ *
+ * @author djaghloul
+ */
+class taoEventModelConverter {
+
+    public $currentTrace = '';
+
+    public function __construct($xml) {
+        $this->currentTrace = $xml;
+    }
+
+    //convert a trace done by HAWAI item to Tao Event Model
+    public function importFromHAWAI($xml='') {
+        $unifiedArray = array();
+        $usedXml = $xml;
+        if ($xml == '') {
+            $usedXml = $this->currentTrace;
+        }
+
+        $hawaiTrace = new DOMDocument();
+        $hawaiTrace->loadXML($usedXml);
+//create the unified array bay parsing attribute and string
+        $listEvent = $hawaiTrace->getElementsByTagName('taoEvent');
+
+        $unifiedArray = $this->getTracesAsArray($listEvent);
+        //create the Xml
+        $es = new eventsServices();
+        $finalXml = $es->simpleArrayToXml($unifiedArray, ROOT, EVENT_NODE);
+        return $finalXml;
+    }
+
+    private function getTracesAsArray($domTrace=null) {
+
+        $tracesArray = array();
+        $taoEventList = $domTrace;
+
+
+        //$tracesArray=0;
+        //************************************************
+        foreach ($taoEventList as $taoEvent) {
+            //On commence par extraire les attributs de la trace de l'evenement
+
+            $TE_Name = $taoEvent->getAttribute('Name');
+            $TE_Type = $taoEvent->getAttribute('Type');
+            $TE_Time = $taoEvent->getAttribute('Time');
+
+            //Decoder le XML ou voir la section CDATA,
+            $value = $taoEvent->nodeValue;
+            //echo $value;
+            $T_Values = array();
+            $T_Values = $this->parseEventTraceValue($value);
+
+            //Création du tableau des traces, il englobe toutes les variables ( tableau peyload + tableaux des attributs fixes)
+            //$trace = $T_Values;
+
+            $trace['TE_Name'] = $TE_Name;
+            $trace['TE_Type'] = $TE_Type;
+            $trace['TE_Time'] = $TE_Time;
+
+            //Merge the tow arrays ( attribute + node values )
+            $tout = array_merge($trace, $T_Values);
+            $tracesArray[] = $tout;
+        }
+
+        return $tracesArray;
+    }
+
+    //******************************************************
+    //Cette methode reçoit la valeur du noeud itemValue comme string, et la decode en variable en retournant le type d'evenement
+    private function parseEventTraceValue($itemTraceValue) {
+
+        $tabVarFinal = array();
+        if ($itemTraceValue != '') {
+
+            $itv = $itemTraceValue; //recuperer le string de la valeur;
+            $tokenVar = array();
+            $tabPart = explode(sep, $itv);
+            $tabVarFinal = array();
+            foreach ($tabPart as $part) {
+                //parse_str($part, $tokenVar); //in  each element of the array we have this: var=value, we need to have $tokenVar[var]=value
+                //we remplace pers_str with exploe because it ascapes directely some symbols
+                $varValue = explode('=', $part);
+                $tokenVar[$varValue[0]] = $varValue[1];
+                $tabVarFinal = array_merge($tabVarFinal, $tokenVar);
+            }
+        }
+        print_r($tabVarFinal);
+        return $tabVarFinal;
+    }
+
+}
+
+//test the converion
+$xmlDoc = simplexml_load_file('hawai12.xml');
+$xml = $xmlDoc->asXML();
+$conv = new taoEventModelConverter($xml);
+$import = $conv->importFromHAWAI();
+
+
+file_put_contents('taoHawai.xml', $import);
+?>
