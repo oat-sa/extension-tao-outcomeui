1d88ee0180e4d4c79f9cdc3328d99e6a7b999a45
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-11-04 14:48:11 +0000

    * Add the Recovery implementation in the WfEngine  * align the session methods in the wfEngine
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@3556 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/Server.class.php
+++ actions/Server.class.php
@@ -4,7 +4,7 @@ require_once ('tao/actions/Api.class.php');
 /**
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
- * @package tao
+ * @package taoResults
  * @subpackage action
  *
  */
