5d5570591df3f2e036580779b83e40a3cf11e77b
ydjaghloul <ydjaghloul@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-12 16:42:41 +0000

    Add UTR model as TAO specification
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@4155 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ models/classes/class.TReg_VirtualTable.php
@@ -0,0 +1,305 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * TAO - taoResults\models\classes\class.TReg_VirtualTable.php
+ *
+ * $Id$
+ *
+ * This file is part of TAO.
+ *
+ * Automatically generated on 12.01.2011, 17:22:12 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+ * @package taoResults
+ * @subpackage models_classes
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA5-includes begin
+// section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA5-includes end
+
+/* user defined constants */
+// section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA5-constants begin
+// section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA5-constants end
+
+/**
+ * Short description of class taoResults_models_classes_TReg_VirtualTable
+ *
+ * @access public
+ * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+ * @package taoResults
+ * @subpackage models_classes
+ */
+class taoResults_models_classes_TReg_VirtualTable
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+    /**
+     * Short description of method __construct
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @return mixed
+     */
+    public function __construct()
+    {
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA6 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA6 end
+    }
+
+    /**
+     * Short description of method trDeleteRow
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  string uriInstance
+     * @param  array listInstances
+     * @return array
+     */
+    public function trDeleteRow($uriInstance, $listInstances)
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA8 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AA8 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method array
+     *
+     * @access private
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array listRows
+     * @param  array listInstances
+     * @return mixed
+     */
+    private function array($listRows, $listInstances)
+    {
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AAC begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AAC end
+    }
+
+    /**
+     * Short description of method deleteColumn
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  string $columnId
+     * @param  array utrModel
+     * @return array
+     */
+    public function deleteColumn($$columnId, $utrModel)
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AB0 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AB0 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method generateUTR
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array utrModel
+     * @param  array listInstances
+     * @return array
+     */
+    public function generateUTR($utrModel, $listInstances)
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AB4 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AB4 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method trGetInstances
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @return array
+     */
+    public function trGetInstances()
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AB8 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AB8 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method getClassesOfinstances
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array uriInstances
+     * @return array
+     */
+    public function getClassesOfinstances($uriInstances)
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ABA begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ABA end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method addColumn
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array columnDescription
+     * @param  array utrModel
+     * @return array
+     */
+    public function addColumn($columnDescription, $utrModel)
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ABD begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ABD end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method saveUtrModel
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array utrModel
+     * @param  string idModel
+     * @return string
+     */
+    public function saveUtrModel($utrModel, $idModel)
+    {
+        $returnValue = (string) '';
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AC1 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AC1 end
+
+        return (string) $returnValue;
+    }
+
+    /**
+     * Short description of method loadUtrModel
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  string idModel
+     * @return string
+     */
+    public function loadUtrModel($idModel)
+    {
+        $returnValue = (string) '';
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AC5 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AC5 end
+
+        return (string) $returnValue;
+    }
+
+    /**
+     * Short description of method getListOfUtrModel
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @return array
+     */
+    public function getListOfUtrModel()
+    {
+        $returnValue = array();
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AC8 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AC8 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method createSimpleUtr
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array listOfInstances
+     * @param  array listOfProperties
+     * @return mixed
+     */
+    public function createSimpleUtr($listOfInstances, $listOfProperties)
+    {
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ACB begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ACB end
+    }
+
+    /**
+     * Short description of method trGetCurrentExtention
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @return string
+     */
+    public function trGetCurrentExtention()
+    {
+        $returnValue = (string) '';
+
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ACF begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002ACF end
+
+        return (string) $returnValue;
+    }
+
+    /**
+     * Short description of method exportCSV
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  array utrTable
+     * @param  char del
+     * @return mixed
+     */
+    public function exportCSV($utrTable, $del)
+    {
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AD1 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AD1 end
+    }
+
+    /**
+     * Short description of method exportToExcel
+     *
+     * @access public
+     * @author Younes Djaghloul, <younes.djaghloul@tudor.lu>
+     * @param  void utrTable
+     * @return mixed
+     */
+    public function exportToExcel( core_kernel_classes_Session_void $utrTable)
+    {
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AD5 begin
+        // section 10-13-1--65-3b6a288d:12d79aedebf:-8000:0000000000002AD5 end
+    }
+
+} /* end of class taoResults_models_classes_TReg_VirtualTable */
+
+?>
