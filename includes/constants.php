d9eed78d9de41518c5b5b39001b105dee68aaea3
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-28 14:01:25 +0000

    * CLEAN DATABASE  * UPDATE WAY OF CONTSTANT LOADING  * Minor fix in the wfEngine  * Add type attr by default in the QTI object tag
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@4370 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -5,9 +5,6 @@
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  *
  */
-
-include_once ROOT_PATH . '/tao/includes/constants.php';
-
 $todefine = array(
 	'RESULT_ONTOLOGY'		=> 'http://www.tao.lu/Ontologies/TAOResult.rdf',
 	'ITEM_ONTOLOGY'			=> 'http://www.tao.lu/Ontologies/TAOItem.rdf',
@@ -32,11 +29,4 @@ $todefine = array(
 	'PROP_RESULT_ITEM_VARIABLE_ID' => 'http://www.tao.lu/Ontologies/TAOResult.rdf#TAO_ITEM_VARIABLE_ID',
 	'PROP_RESULT_ITEM_VARIABLE_VALUE' => 'http://www.tao.lu/Ontologies/TAOResult.rdf#TAO_ITEM_VARIABLE_VALUE'
 );
-
-foreach($todefine as $constName => $constValue){
-	if(!defined($constName)){
-		define($constName, $constValue);
-	}
-}
-unset($todefine);
 ?>
