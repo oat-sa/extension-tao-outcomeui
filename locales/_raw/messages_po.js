b118318fd07ddde5c413ea6259403718f021d5cd
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-07-26 11:59:25 +0000

    PO Update
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@6501 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ locales/_raw/messages_po.js
@@ -0,0 +1,4 @@
+/* auto generated content */
+/* extesion: taoResults, lang: _raw */
+var langCode = '_raw';
+var i18n_tr = {"All results of passed Tests with their referring Subject, Group and Item specific data, as well as the individual data collected during Test execution are stored and managed here.":"","Class saved":"","Edit result":"","Edit result class":"","Manage results":"","Result saved":"","Results":"","Results library":"","Template Saved":"","This module constitutes the final step.":"","create direct table":"","create score table":"","create table":""};
