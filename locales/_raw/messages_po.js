9f92f01a31858cdeb609c924b929a2e96369cc32
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-01-18 15:44:38 +0000

    Locales generated with new translation scripts.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@8319 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/_raw/messages_po.js
+++ locales/_raw/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
-/* extesion: taoResults, lang: _raw */
+/* lang: _raw */
 var langCode = '_raw';
-var i18n_tr = {"All results of passed Tests with their referring Subject, Group and Item specific data, as well as the individual data collected during Test execution are stored and managed here.":"","Class saved":"","Edit result":"","Edit result class":"","Manage results":"","Result saved":"","Results":"","Results library":"","Template Saved":"","This module constitutes the final step.":"","create direct table":"","create score table":"","create table":""};
+var i18n_tr = {"All results of passed Tests with their referring Subject, Group and Item specific data, as well as the individual data collected during Test execution are stored and managed here.":"","Class saved":"","create direct table":"","create score table":"","create table":"","delete":"","Edit result":"","Edit result class":"","Manage results":"","Result saved":"","Results":"","Results library":"","Template Saved":"","This module constitutes the final step.":"","View and format the collected results.":""};
