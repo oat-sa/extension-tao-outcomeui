ad13d1bdd6c79fee51819b4a89627654541bfb44
jbihin <jbihin@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-03-07 14:05:35 +0000

    adding functionality ACL adding ACL files for each extension funcACL RDF script generator modify manifest fort installing multiple file for namespace taoCoding  adding conciliation
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@9975 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -3,7 +3,7 @@
 /*
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
- * 
+ *
  */
 	return array(
 		'name' => 'taoResults',
@@ -12,17 +12,21 @@
 			'version' => '2.0',
 			'author' => 'CRP Henri Tudor',
 			'dependances' => array('tao'),
-			'models' => 'http://www.tao.lu/Ontologies/TAOResult.rdf',
-			'install' => array( 
-				'rdf' => dirname(__FILE__). '/models/ontology/taoresult.rdf'
+			'models' => array('http://www.tao.lu/Ontologies/TAOResult.rdf',
+				'http://www.tao.lu/Ontologies/taoFuncACL.rdf'),
+			'install' => array(
+				'rdf' => array(
+						array('ns' => 'http://www.tao.lu/Ontologies/TAOResult.rdf', 'file' => dirname(__FILE__). '/models/ontology/taoresult.rdf'),
+						array('ns' => 'http://www.tao.lu/Ontologies/taoFuncACL.rdf', 'file' => dirname(__FILE__). '/models/ontology/funcacl.rdf')
+				)
 			),
-			'classLoaderPackages' => array( 
+			'classLoaderPackages' => array(
 				dirname(__FILE__).'/actions/',
 				dirname(__FILE__).'/helpers/'
 			 )
 
-				
-			
+
+
 		)
 	);
 ?>
