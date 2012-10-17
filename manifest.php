595c7c141dc35c1d76b240066b539eef510337f4
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-10-17 13:10:08 +0000

    manifest.php -> version goes from 2.3 to 2.4
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@12454 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -10,7 +10,7 @@ $taopath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'tao'.DIRECTORY_SEPARA
 return array(
 	'name' => 'taoResults',
 	'description' => 'TAO Results extension',
-	'version' => '2.3',
+	'version' => '2.4',
 	'author' => 'CRP Henri Tudor',
 	'dependencies' => array('taoTests'),
 	'models' => array('http://www.tao.lu/Ontologies/TAOResult.rdf',
