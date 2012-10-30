a7dca7ad63d217722aa0ea6bc4f7cd22c74577d3
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-10-30 13:14:46 +0000

    final move of wfEngine authoring part to wfAuthoring
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@12626 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.DeliveryResultGrid.php
+++ helpers/class.DeliveryResultGrid.php
@@ -3,14 +3,11 @@
 error_reporting(E_ALL);
 
 /**
- * TAO - wfEngine/helpers/Monitoring/class.ProcessMonitoringGrid.php
+ * TAO - taoResults/helpers/class.DeliveryResultGrid.php
  *
  * $Id$
  *
  * This file is part of TAO.
- *
- * Automatically generated on 16.11.2011, 10:26:42 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
  *
  * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
  * @package wfEngine
@@ -29,15 +26,11 @@ if (0 > version_compare(PHP_VERSION, '5')) {
 require_once('tao/helpers/grid/class.GridContainer.php');
 
 /* user defined includes */
-// section 127-0-1-1--17d909f0:1336f22bf6e:-8000:00000000000032D7-includes begin
-// section 127-0-1-1--17d909f0:1336f22bf6e:-8000:00000000000032D7-includes end
 
 /* user defined constants */
-// section 127-0-1-1--17d909f0:1336f22bf6e:-8000:00000000000032D7-constants begin
-// section 127-0-1-1--17d909f0:1336f22bf6e:-8000:00000000000032D7-constants end
 
 /**
- * Short description of class wfEngine_helpers_Monitoring_ProcessMonitoringGrid
+ * Short description of class taoResults_helpers_DeliveryResultGrid
  *
  * @access public
  * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
@@ -73,8 +66,6 @@ class taoResults_helpers_DeliveryResultGrid
     {
         $returnValue = (bool) false;
 
-        // section 127-0-1-1--521607b6:1338265e839:-8000:000000000000335C begin
-		
 		$excludedProperties = (is_array($this->options) && isset($this->options['excludedProperties']))?$this->options['excludedProperties']:array();
 		$columnNames = (is_array($this->options) && isset($this->options['columnNames']))?$this->options['columnNames']:array();
 		
@@ -94,12 +85,11 @@ class taoResults_helpers_DeliveryResultGrid
 			array_keys($processProperties),
 			new tao_helpers_grid_Cell_ResourceLabelAdapter()
 		);
-        // section 127-0-1-1--521607b6:1338265e839:-8000:000000000000335C end
 
         return (bool) $returnValue;
     }
 
 
-} /* end of class wfEngine_helpers_Monitoring_ProcessMonitoringGrid */
+} /* end of class taoResults_helpers_DeliveryResultGrid*/
 
 ?>
