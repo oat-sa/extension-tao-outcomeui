c9ee4d8e6d265b35aebbcc56492ec97aa72b80d5
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-10-14 15:05:55 +0000

    download issue for traces solved
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@16762 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/viewResult.js
+++ views/js/viewResult.js
@@ -1,9 +1,11 @@
 
-$(function () {
-    require(['require', 'jquery', '/taoResults/views/js/viewResult.js',root_url  + 'tao/views/js/jquery.fileDownload.js'], function () {
+$(document).ready(function () {
+    require(['require', 'jquery', root_url  + 'tao/views/js/jquery.fileDownload.js'],
+    function () {
+
 	$('#filter').val(filter);
 	$('.dataResult').html(function(index, oldhtml) {
-	    
+
 	    return oldhtml;
 	    });
 	$('#filter').change(function(e) {
@@ -11,41 +13,24 @@ $(function () {
 	    data.filter = $( this ).val();
 	    helpers._load(helpers.getMainContainerSelector(uiBootstrap.tabs), url, data);
 	    });
-	    
-	    });
-	$('.traceDownload').click(function (e) {
-	  var variableUri = $(this).val();
-	  $.fileDownload(root_url + 'taoResults/Results/getTrace', {
+
+
+
+
+
+	$('.traceDownload').on("click", function (e) {
+	    var variableUri = $(this).val();
+	    $.fileDownload(root_url + 'taoResults/Results/getTrace', {
 	      preparingMessageHtml: __("We are preparing your report, please wait..."),
 	      failMessageHtml: __("There was a problem generating your report, please try again."),
 	      successCallback: function () { },
 	      httpMethod: "POST",
 	       ////This gives the current selection of filters (facet based query) and the list of columns selected from the client (the list of columns is not kept on the server side class.taoTable.php
 	      data: {'variableUri': variableUri}
-	  });
+	    });
 	});
-	
+
+
 
     });
-/*
-    function layoutResponse(data){
-	var formattedData = "";
-	//the data may be not valid json, in this case there is a silent fail and the data is returned.
-	try{
-	var jsData = $.parseJSON(data);
-	if (jsData instanceof Array) {
-	    formattedData = '<OL >';
-	    for (key in jsData){
-		formattedData += '<li >';
-		formattedData += jsData[key];
-		 formattedData += "</li>";
-		}
-	     formattedData += "</OL>";
-	} else {
-	    formattedData = data;
-	    }
-	}
-	catch(err){formattedData = data;}
-	return formattedData;
-	}
-	*/
+       });
