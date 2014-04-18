9c09c97168c478ff4606c905e15e0bb689b55c36
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-18 09:36:10 +0000

    to be tested when the update process will eb ready
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@19794 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/update/taoResultsUpdate.php
+++ scripts/update/taoResultsUpdate.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once dirname(__FILE__) .'/../includes/raw_start.php';
+require_once dirname(__FILE__) .'/../../includes/raw_start.php';
 
-new taoResults_update_taoResultsUpdate(array('parameters' => array()));
+new taoResults_scripts_update_taoResultsUpdate(array('parameters' => array()));
 
 
 ?>
