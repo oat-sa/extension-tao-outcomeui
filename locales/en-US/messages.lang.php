69985465e1586f2d38f3b6fa253b049fb9faa12a
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-05 15:21:29 +0000

    changed description (ran taoTranslate.php -v -a compile -l en-US -e taoResults and update )
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResults/trunk@20067 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages.lang.php
+++ locales/en-US/messages.lang.php
@@ -1,3 +1,4 @@
 <?php
+$GLOBALS['__l10n']["All results of passed Tests with their referring Subject, Group and Item specific data, as well as the individual data collected during Test execution are stored and managed here."]="Store, manage, and analyze test results including subject, group and item specific data, as well as data collected about each individual test-taker. Export test results into data warehouses for long-term analysis and reporting.";
 
 ?>
--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : []
+   i18n_tr : {"All results of passed Tests with their referring Subject, Group and Item specific data, as well as the individual data collected during Test execution are stored and managed here.":"Store, manage, and analyze test results including subject, group and item specific data, as well as data collected about each individual test-taker. Export test results into data warehouses for long-term analysis and reporting."}
  };
 });
