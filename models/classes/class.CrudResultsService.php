e372fdedf039535d840e6daadc26ecf4c4d1b291
Lionel Lecaque <lionel@taotesting.com>
2014-09-08 18:10:04 +0200

    change crudService signature

--- models/classes/class.CrudResultsService.php
+++ models/classes/class.CrudResultsService.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -15,63 +15,77 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
+ * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA
  * 
  */
 
 /**
- * .Crud services implements basic CRUD services, orginally intended for REST controllers/ HTTP exception handlers
- *  Consequently the signatures and behaviors is closer to REST and throwing HTTP like exceptions
- *  
- *
+ * Crud services implements basic CRUD services, orginally intended for 
+ * REST controllers/ HTTP exception handlers.
+ * Consequently the signatures and behaviors is closer to REST and throwing HTTP like exceptions
  * 
+ * @author Patrick Plichart, patrick@taotesting.com
  */
-class taoResults_models_classes_CrudResultsService extends tao_models_classes_CrudService {
+class taoResults_models_classes_CrudResultsService extends tao_models_classes_CrudService
+{
 
-    protected $resultClass = null;
-    protected $resultService = null;
-
-    public function __construct() {
-        parent::__construct();
-        $this->resultClass = new core_kernel_classes_Class(TAO_DELIVERY_RESULT);
-        $this->resultService = taoResults_models_classes_ResultsService::singleton();
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::getClassService()
+     */
+    protected function getClassService()
+    {
+        return taoResults_models_classes_ResultsService::singleton();
     }
 
-    public function getRootClass() {
-        return $this->resultClass;
-    }
-
-    public function get($uri) {
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::get()
+     */
+    public function get($uri)
+    {
         $returnData = array();
-        foreach ($this->resultService->getVariables(new core_kernel_classes_Resource($uri), null, false) as $itemVariable => $variables) {
-        
-            foreach ($variables as $key=>$variable) {
-                $returnData[$itemVariable][]=$this->resultService->getVariableData($variable);
+        foreach ($this->getClassService()->getVariables(new core_kernel_classes_Resource($uri), null, false) as $itemVariable => $variables) {
+            
+            foreach ($variables as $key => $variable) {
+                $returnData[$itemVariable][] = $this->getClassService()->getVariableData($variable);
             }
-        };
+        }
+        
         return $returnData;
-        //return $this->resultService->getVariables(new core_kernel_classes_Resource($uri));
     }
 
-    public function getAll() {
-        return parent::getAll();
-    }
-
-    public function delete($resource) {
-       throw new common_exception_NoImplementation();
-        return true;
-    }
-
-    public function deleteAll() {
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::delete()
+     */
+    public function delete($resource)
+    {
         throw new common_exception_NoImplementation();
     }
 
-   
-
-    public function update($uri = null, $propertiesValues = array()) {
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::deleteAll()
+     */
+    public function deleteAll()
+    {
         throw new common_exception_NoImplementation();
-        //throw new common_exception_NotImplemented();
     }
 
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::update()
+     */
+    public function update($uri = null, $propertiesValues = array())
+    {
+        throw new common_exception_NoImplementation();
+    }
 }
 
 ?>
