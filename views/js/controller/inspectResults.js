e14b6dfc4efff899852f7599c43fb2dadf592ec9
hutnikau <hutnikau@1pt.com>
2015-07-15 14:51:57 +0300

    add finish expired tests button

--- views/js/controller/inspectResults.js
+++ views/js/controller/inspectResults.js
@@ -87,9 +87,20 @@ define([
          */
         start : function(){
             var $container = $('#inspect-result');
-            
             //load results also at the beginning unfiltered
             loadResults($container, $container.data('model'), $container.data('uri'));
+            
+            binder.register('endExpiredTests', function () {
+                var url = this.url;
+                $.ajax({
+                    type : 'GET',
+                    url : url,
+                    dataType : 'json',
+                    success : function (data) {
+                        feedback().info(__('%s tests has been finished.', data.length.toString()));
+                    }
+                });
+            });
         }
     };
 
