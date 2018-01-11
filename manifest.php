4376465ae6a9717f28d31c34fa498242ef2aff4a
Ionut Padurariu <ionut@taotesting.com>
2018-01-11 15:45:04 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return [
     'label'          => 'Result visualisation',
     'description'    => 'TAO Results extension',
     'license'        => 'GPL-2.0',
-    'version'        => '5.2.0',
+    'version'        => '5.2.1',
     'author'         => 'Open Assessment Technologies, CRP Henri Tudor',
     // taoItems is only needed for the item model property retrieval
     'requires'       => [
