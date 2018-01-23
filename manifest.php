166b6ea43d50f5ddb48ca0bbd07968d12398c90d
Aleksej Tihanovich <tihanovich.a@gmail.com>
2018-01-23 13:28:29 +0300

    Bump version

--- manifest.php
+++ manifest.php
@@ -31,14 +31,14 @@ return [
     'label'          => 'Result visualisation',
     'description'    => 'TAO Results extension',
     'license'        => 'GPL-2.0',
-    'version'        => '5.2.1',
+    'version'        => '5.3.0',
     'author'         => 'Open Assessment Technologies, CRP Henri Tudor',
     // taoItems is only needed for the item model property retrieval
     'requires'       => [
         'taoResultServer' => '>=5.1.0',
         'taoItems'        => '>=5.2.0',
         'taoDeliveryRdf'  => '>=3.24.0',
-        'tao'             => '>=14.11.0',
+        'tao'             => '>=15.7.0',
         'taoTaskQueue'    => '>=0.11.0'
     ],
     'install'        => [
