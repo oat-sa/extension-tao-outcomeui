a47bfbd75c73614af17ad7deee73417f234290d4
Ferenc Kiss <ferenc@taotesting.com>
2019-01-30 13:20:02 +0100

    TAO-6785 - Version bump fix

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -171,7 +171,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('5.13.0');
         }
 
-        $this->skip('5.13.0', '7.4.4');
+        $this->skip('5.13.0', '7.4.3');
 
         if ($this->isVersion('7.4.3')) {
             /** @var EventManager $eventManager */
