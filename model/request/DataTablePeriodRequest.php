d94ae6ce9622e9f1f03affcf155ab61815072527
Ihar Aleksiaichuk <ihar.aleksiaichuk@1pt.com>
2019-03-04 14:47:51 +0300

    add constructor

--- model/request/DataTablePeriodRequest.php
+++ model/request/DataTablePeriodRequest.php
@@ -3,6 +3,7 @@
 namespace oat\taoOutcomeUi\model\request;
 use oat\tao\model\datatable\implementation\DatatableRequest;
 use oat\taoOutcomeUi\model\ResultsService;
+use Psr\Http\Message\ServerRequestInterface;
 
 class DataTablePeriodRequest extends DatatableRequest
 {
@@ -11,17 +12,32 @@ class DataTablePeriodRequest extends DatatableRequest
     private $requestParams;
 
     /**
+     * DatatableRequest constructor.
+     * @param ServerRequestInterface $request
+     */
+    public function __construct(ServerRequestInterface $request)
+    {
+        $bodyParams = $request->getParsedBody();
+        if ($bodyParams === null) {
+            $bodyParams = [];
+        }
+        $queryParams = $request->getQueryParams();
+        $this->requestParams = array_merge($bodyParams, $queryParams);
+    }
+    /**
      * Get periodFilter
      * @return array
      */
     public function periodFilter()
     {
+
         $period = [];
         foreach (self::PERIOD_FILTER as $value){
             if(isset($this->requestParams[$value])){
                 $period[$value] = strtotime($this->requestParams[$value]);
             }
-        }
+         }
+
         return $period;
     }
 }
