fe1a90b6821686f5df202f8f3c520bb25b156d58
Pavel Handleman <pavel.handleman@gmail.com>
2019-03-19 17:23:58 +0300

    version bump

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return [
     'label'          => 'Result visualisation',
     'description'    => 'TAO Results extension',
     'license'        => 'GPL-2.0',
-    'version'        => '7.5.5',
+    'version'        => '7.6.0',
     'author'         => 'Open Assessment Technologies, CRP Henri Tudor',
     // taoItems is only needed for the item model property retrieval
     'requires'       => [
@@ -45,7 +45,7 @@ return [
         'taoResultServer' => '>=7.0.0',
         'taoItems'        => '>=6.0.0',
         'taoDeliveryRdf'  => '>=6.0.0',
-        'tao'             => '>=21.15.0'
+        'tao'             => '>=30.0.0'
     ],
     'install'        => [
         'php' => [
