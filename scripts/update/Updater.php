0273ed645000066a551e5aa0f68b2d0bde09934e
Bout <joel@taotesting.com>
2020-05-15 10:37:06 +0200

    Bump v8.7.3

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -182,7 +182,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('7.5.0');
         }
 
-        $this->skip('7.5.0', '8.7.2');
+        $this->skip('7.5.0', '8.7.3');
 
     }
 }
