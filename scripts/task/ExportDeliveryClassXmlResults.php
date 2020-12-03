5b1913a45283e2d1b38829ce587a2ba98abbe45e
aliak <aliakeiv@taotesting.com>
2020-12-03 17:14:09 +0300

    fixed PR comments

--- scripts/task/ExportDeliveryClassXmlResults.php
+++ scripts/task/ExportDeliveryClassXmlResults.php
@@ -20,6 +20,8 @@ class ExportDeliveryClassXmlResults extends ScriptAction
 {
     use OntologyAwareTrait;
 
+    private const ITERATIONS_TO_RELOAD = 100;
+
     /**
      * @var Report
      */
@@ -66,27 +68,27 @@ class ExportDeliveryClassXmlResults extends ScriptAction
         $deliveryClass = $this->getClass($class);
         $resultCount = 0;
 
-        $fileName = \tao_helpers_Display::textCleaner($zipFilenamePrefix) .'-'. time() . '.zip';
-        $path = \tao_helpers_File::concat([\tao_helpers_Export::getExportPath(), $fileName]);
+        try {
+            $fileName = \tao_helpers_Display::textCleaner($zipFilenamePrefix) . '-' . time() . '.zip';
+            $path = \tao_helpers_File::concat([\tao_helpers_Export::getExportPath(), $fileName]);
 
-        if (!\tao_helpers_File::securityCheck($path, true)) {
-            throw new \Exception('Unauthorized file name');
-        }
-        if (file_exists($path)) {
-            unlink($path);
-        }
-        $zipArchive = new \ZipArchive();
-        if ($zipArchive->open($path, \ZipArchive::CREATE) !== true) {
-            throw new \Exception('Unable to create archive at ' . $path);
-        }
+            if (!\tao_helpers_File::securityCheck($path, true)) {
+                throw new \Exception('Unauthorized file name');
+            }
+            if (file_exists($path)) {
+                unlink($path);
+            }
+            $zipArchive = new \ZipArchive();
+            if ($zipArchive->open($path, \ZipArchive::CREATE) !== true) {
+                throw new \Exception('Unable to create archive at ' . $path);
+            }
 
-        $deliveryNumber = 0;
-        $executionAcrossDeliveriesNumber = 0;
-        foreach ($deliveryClass->getInstances(true) as $delivery) {
-            $deliveryNumber++;
-            $this->report->add(Report::createInfo(sprintf('Starting to process delivery "%s"', $delivery->getLabel())));
+            $deliveryNumber = 0;
+            $executionAcrossDeliveriesNumber = 0;
+            foreach ($deliveryClass->getInstances(true) as $delivery) {
+                $deliveryNumber++;
+                $this->report->add(Report::createInfo(sprintf('Starting to process delivery "%s"', $delivery->getLabel())));
 
-            try {
                 $executions = $this->getResultsService()->getResultsByDelivery($delivery);
 
                 $this->report->add(Report::createInfo(sprintf('%d results were found for delivery "%s"', count($executions), $delivery->getLabel())));
@@ -98,7 +100,7 @@ class ExportDeliveryClassXmlResults extends ScriptAction
 
                     // flush the archive by creating the object ZipArchive from scratch every 100 iterations to
                     // prevent ZipArchive from memory leaking (it builds up all the changes in memory that are not 'committed')
-                    if ($executionAcrossDeliveriesNumber % 100 === 0) {
+                    if ($executionAcrossDeliveriesNumber % self::ITERATIONS_TO_RELOAD === 0) {
                         $zipArchive->close();
                         $zipArchive = new \ZipArchive();
 
@@ -116,12 +118,12 @@ class ExportDeliveryClassXmlResults extends ScriptAction
                     $zipArchive->addFromString('results/' . $delivery->getLabel() . '/' . $xmlfileName, $xml);
                     $resultCount++;
                 }
-            } catch (\Exception $e) {
-                if (isset($execution)) {
-                    $this->report->add(Report::createFailure(sprintf('Exception thrown while processing execution "%s": %s', $execution, $e->getMessage())));
-                } else {
-                    $this->report->add(Report::createFailure($e->getMessage()));
-                }
+            }
+        } catch (\Throwable $e) {
+            if (isset($execution)) {
+                $this->report->add(Report::createFailure(sprintf('Exception thrown while processing execution "%s": %s', $execution, $e->getMessage())));
+            } else {
+                $this->report->add(Report::createFailure($e->getMessage()));
             }
         }
         $this->report->add(Report::createSuccess(sprintf('Done! %d results for %d deliveries were exported. Please download the ZIP file at this path: %s', $resultCount, $deliveryNumber, $path)));
