a8ee0da5d6dc5f0671f8f1e5aa56e791d6298bb7
Aleh Hutnikau <hutnikau@1pt.com>
2021-02-18 11:01:40 +0300

    chore!: move dependencies to composer.json

--- manifest.php
+++ manifest.php
@@ -39,16 +39,7 @@ return [
     'label'          => 'Result visualisation',
     'description'    => 'TAO Results extension',
     'license'        => 'GPL-2.0',
-    'version'        => '9.5.0',
     'author'         => 'Open Assessment Technologies, CRP Henri Tudor',
-    // taoItems is only needed for the item model property retrieval
-    'requires'       => [
-        'generis'         => '>=12.15.0',
-        'taoResultServer' => '>=12.3.0',
-        'taoItems'        => '>=6.0.0',
-        'taoDeliveryRdf'  => '>=6.0.0',
-        'tao'             => '>=36.1.0'
-    ],
     'install'        => [
         'php' => [
             RegisterTestPluginService::class,
